e984929bf81476df27784cd8cf5e56a6267cc1a2
Carlos Seda <carlosseda123@gmail.com>
2022-08-10 21:53:48 -0500

    all solutions

--- scripts/vowels.py
+++ scripts/vowels.py
@@ -1,1 +1,18 @@
 # Write the solution for "Filter Vowels".
+
+import re 
+
+file = open("./inputs/vowels.txt", "r")
+text = file.read()
+
+my_dict = {x: len(re.findall(f"{x}", text)) for x in "aeiou"}
+
+total = 0
+
+for number, val in my_dict.items():
+    total += val
+
+with open("vowels-" + str(total) + ".txt", "w") as f:
+    f.write(text)
+
+
